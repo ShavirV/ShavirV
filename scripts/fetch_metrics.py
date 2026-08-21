528cb85d53a27f30522d8b703ed2509c301b57b9
Shavir Vallabh <157022367+ShavirV@users.noreply.github.com>
2026-08-21 12:52:26 +0200

    increase commits per repo to see if it makes a difference

--- scripts/fetch_metrics.py
+++ scripts/fetch_metrics.py
@@ -20,7 +20,7 @@ HEADERS = {
 }
 
 # Number of commits used for the LOC calculation per repository.
-COMMITS_PER_REPO = 100
+COMMITS_PER_REPO = 200
 
 # Retry transient GitHub API failures.
 MAX_RETRIES = 3
